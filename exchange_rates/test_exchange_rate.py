92345b3237730fb64fcf5a029cb174edf771a317
clara <10088152+claraj@users.noreply.github.com>
2020-10-03 16:23:37 -0500

    spacing and added note on test data dictionary

--- exchange_rates/test_exchange_rate.py
+++ exchange_rates/test_exchange_rate.py
@@ -9,6 +9,7 @@ class TestExchangeRates(TestCase):
     @patch('exchange_rate.request_rates')
     def test_dollars_to_target(self, mock_rates):
         mock_rate = 12.34567   # Any number will do.  
+        # As long as the JSON contains the data the program needs, it does not need to be a complete response
         example_api_response = {'base': 'USD', 'date': '2019-02-04', 'rates': {'EUR': mock_rate}}  
         mock_rates.side_effect = [ example_api_response ] 
         # 100 dollars is 1234.567 Euros at this made up exchange rate 
